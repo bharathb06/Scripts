a32caa07612f4af6ce3c88aaeee53b0beaa28ffd
d0landuck06 <d0landucky06@gmail.com>
2020-04-08 19:48:03 +0530

    day2

---
+++ bash/tutorial/var1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+msg="HELLO WORLD"
+echo $msg
+
+x="hello"
+expr $x + 1
+
+echo "enter your reddit karma "
+read user_karma
+echo "HAHA mine is 69 but yours is $user_karma "
+
+
+
---
+++ bash/tutorial/var2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "MYVAR is: $MYVAR"
+MYVAR="hi there"
+echo "MYVAR is: $MYVAR"
+
+
+echo "MYVAR1 is: $MYVAR1"
+MYVAR1="hi there1"
+echo "MYVAR1 is: $MYVAR1"
+
+
+
+# export : to export a variable for it to be inherited by another pgm
+# . command to source a script     -> .profile and .bash_profile work
+
